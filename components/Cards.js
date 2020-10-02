32c1fc5b66bd0dcf1da93ec4711d483462c12e7e
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-10-02 12:05:30 -0500

    mvp

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,72 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+ 
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(res => {
+    const jsData = res.data.articles.javascript
+    jsData.forEach(elem =>{
+        cardsContainer.appendChild(articleCard(elem))
+        })
+
+    const bsData = res.data.articles.bootstrap
+    bsData.forEach(elem =>{
+        cardsContainer.appendChild(articleCard(elem))
+    })
+
+    const techData = res.data.articles.technology
+    techData.forEach(elem =>{
+        cardsContainer.appendChild(articleCard(elem))
+    })
+
+    const jqData = res.data.articles.jquery
+    jqData.forEach(elem =>{
+        cardsContainer.appendChild(articleCard(elem))
+    })
+
+    const nodeData = res.data.articles.node
+    nodeData.forEach(elem =>{
+        cardsContainer.appendChild(articleCard(elem))
+    })
+})
+
+
+
+.catch(error =>{
+    debugger
+})
+
+function articleCard(article) {
+
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authorName = document.createElement('span')
+
+    headline.textContent = article.headline
+    authorImg.src = article.authorPhoto
+    authorName.textContent = `By: ${article.authorName}`
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(authorImg)
+    author.appendChild(authorName)
+
+    card.addEventListener('click', (e) => {
+        console.log(headline.textContent)
+    })
+
+    return card
+}
+
+const cardsContainer = document.querySelector('.cards-container')
+
+console.log (cardsContainer)
